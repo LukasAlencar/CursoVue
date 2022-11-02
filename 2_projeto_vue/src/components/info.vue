857b1fd8d7ef1fc429eda08a7bf3df44d35184ad
LukasAlencar <lucas.kauan@aluno.ifsp.edu.br>
2022-11-02 20:33:46 -0300

    v-for

--- 2_projeto_vue/src/components/info.vue
+++ 2_projeto_vue/src/components/info.vue
@@ -2,12 +2,15 @@
     <div>
         <p v-if="estaTrabalhando">Estou trabalhando no momento.</p>
         <p v-else>Não estou trabalhando</p>
-        <p>Utilizo as seguintes tecnologias:</p>
+        <p>Utilizo as seguintes tecnologias para backend:</p>
+        <ul >
+            <li v-for="(technology, index) in backendTechnologies" v-bind:key="index"> {{technology}} </li>
+        </ul>
+        <p>Utilizo as seguintes tecnologias para FrontEnd</p>
         <ul>
-            <li>Javascript</li>
-            <li>PHP</li>
-            <li>Java</li>
-            <li>Vue</li>
+            <li v-for="technology in frontEndTechnologies" :key="technology.id">
+                {{ technology.language }}
+            </li>
         </ul>
         <div>
             <button @click="showEmail">Ver e-mail</button>
@@ -27,7 +30,13 @@ export default {
             estaTrabalhando: true,
             mostrarEmail: false,
             email: "testeemail@asda.com",
-            meuLink: "https://google.com.br"
+            meuLink: "https://google.com.br",
+            backendTechnologies: ['JavaScript', 'PHP', "Phyton"],
+            frontEndTechnologies: [
+                {id: 1, language: 'HTML'},
+                {id: 2, language: 'CSS'},
+                {id: 3, language: 'VUE'}
+            ]
         };
     },
     components: { Picture },
